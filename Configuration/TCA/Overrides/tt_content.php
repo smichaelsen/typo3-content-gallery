420df1ef68662cbaa2733236d079fd0b3dcbd27f
Sebastian Michaelsen <sebastian@michaelsen.io>
2015-05-13 11:31:25 +0200

    Add labels and classes for thumbnails

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,6 +1,26 @@
 <?php
 
 $tempColumns = array(
+	'tx_contentgallery_class' => array(
+		'label' => 'LLL:EXT:content_gallery/Resources/Private/Language/locallang_be.xlf:tt_content.tx_contentgallery_class',
+		'displayCond' => 'FIELD:colPos:=:-1',
+		'config' => array(
+			'type' => 'select',
+			'items' => array(
+				array('', 'default'),
+				array('360°', 'threesixty'),
+				array('Image', 'image'),
+				array('Video', 'video'),
+			),
+		),
+	),
+	'tx_contentgallery_label' => array(
+		'label' => 'LLL:EXT:content_gallery/Resources/Private/Language/locallang_be.xlf:tt_content.tx_contentgallery_label',
+		'displayCond' => 'FIELD:colPos:=:-1',
+		'config' => array(
+			'type' => 'input',
+		),
+	),
 	'tx_contentgallery_thumbnail' => array(
 		'label' => 'LLL:EXT:content_gallery/Resources/Private/Language/locallang_be.xlf:tt_content.tx_contentgallery_thumbnail',
 		'displayCond' => 'FIELD:colPos:=:-1',
@@ -8,4 +28,4 @@ $tempColumns = array(
 	),
 );
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $tempColumns);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', 'tx_contentgallery_thumbnail', '', 'after:header');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', 'tx_contentgallery_thumbnail,tx_contentgallery_label,tx_contentgallery_class', '', 'after:header');
--- ext_tables.sql
+++ ext_tables.sql
@@ -2,5 +2,7 @@
 # Table structure for table 'tt_content'
 #
 CREATE TABLE tt_content (
+	tx_contentgallery_class varchar(11) DEFAULT '' NOT NULL,
+	tx_contentgallery_label varchar(11) DEFAULT '' NOT NULL,
 	tx_contentgallery_thumbnail int(11) DEFAULT '0' NOT NULL,
 );
