69c4a07a011769ec66fd2673230748ad708c1f5a
Sebastian Michaelsen <sebastian@michaelsen.io>
2015-05-13 09:05:51 +0200

    Finish gridelement

--- Configuration/TypoScript/Gridelements/ContentGalleryGridelement.ts
+++ Configuration/TypoScript/Gridelements/ContentGalleryGridelement.ts
@@ -3,9 +3,13 @@ tt_content.gridelements_pi1.20.10.setup {
 	contentGallery {
 		columns.default.renderObj {
 			10 = FLUIDTEMPLATE
-			10.file = EXT:content_gallery/Resources/Private/Templates/Gridelements/ContentGallery.html
+			10 {
+				file = EXT:content_gallery/Resources/Private/Templates/Gridelements/ContentGallery.html
+				partialRootPaths.0 = {$plugin.tx_contentgallery.view.partialRootPath}ContentGallery/
+				layoutRootPaths.0 = {$plugin.tx_contentgallery.view.layoutRootPath}ContentGallery/
+			}
 			20 >
 		}
-		wrap = <div class="gridelement-contentgallery">|</div>
+		wrap = <div class="gridelement-contentgallery"><div class="gridelement-contentgallery-canvas"></div><div class="gridelement-contentgallery-thumbnails row">|</div></div>
 	}
 }
---
+++ Resources/Public/Javascript/contentgallery.js
@@ -0,0 +1,37 @@
+(function($) {
+
+	$.fn.contentGallery = function() {
+		return this.each(function() {
+			var container = $(this);
+			var canvas = container.find('.gridelement-contentgallery-canvas');
+			var elements = container.find('.gridelement-contentgallery-thumbnails').find('.contentgallery-element');
+
+			// initialize markup
+			(function(){
+				elements.each(function(){
+					var element = $(this);
+					var elementContent = element.find('.contentgallery-content');
+					var thumbnail = element.find('.contentgallery-thumbnail');
+					thumbnail.data('elementContent', elementContent);
+					elementContent.appendTo(canvas);
+				});
+			}());
+
+			var elementContents = canvas.find('.contentgallery-content');
+
+			// thumbnails click event
+			(function(){
+				elements.find('.contentgallery-thumbnail').on('click', function(){
+					elementContents.hide();
+					$(this).data('elementContent').show();
+				}).first().click();
+			}());
+
+		});
+	};
+
+	$(function() {
+		$('.gridelement-contentgallery').contentGallery();
+	});
+
+}(jQuery));
